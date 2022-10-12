34b70cca76deafd6ec2c6254f2b8948ebdbb39a7
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-12 21:00:35 +0300

    calculator

---
+++ Untitled11.cpp
@@ -0,0 +1,126 @@
+// C++ program to implement
+// the above approach
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// Class calculator
+class Calculator
+{
+float a, b;
+public:
+
+	// Function to take input
+	// from user
+	void result()
+	{
+		cout << "Enter First Number: ";
+		cin >> a;
+		cout << "Enter Second Number: ";
+		cin >> b;
+	}
+
+	// Function to add two numbers
+	float add()
+	{
+		return a + b;
+	}
+
+	// Function to subtract two numbers
+	float sub()
+	{
+		return a - b;
+	}
+
+	// Function to multiply two numbers
+	float mul()
+	{
+		return a * b;
+	}
+
+	// Function to divide two numbers
+	float div()
+
+{
+	double a, b, Result;
+
+	// Request two numbers from the user
+	cout << "Please provide two numbers\n";
+
+	try {
+		cout << "First Number: ";
+		cin >> a;
+		cout << "Second Number: ";
+		cin >> b;
+
+		if( b == 0 )
+			throw;
+
+		// Perform a division and display the result
+		Result = a/ b;
+		cout << "\n" << a << " / " <<b << " = " << Result << "\n\n";
+	}
+	catch(...)
+	{
+
+	}
+
+	return 0;
+
+}
+
+// Driver code
+int main()
+{
+	int ch;
+	Calculator c;
+	cout << "Enter 1 to Add 2 Numbers" <<
+			"\nEnter 2 to Subtract 2 Numbers" <<
+			"\nEnter 3 to Multiply 2 Numbers" <<
+			"\nEnter 4 to Divide 2 Numbers" <<
+			"\nEnter 0 To Exit";
+		
+	do
+	{
+		cout << "\nEnter Choice: ";
+		cin >> ch;
+		switch (ch)
+		{
+		case 1:
+			
+			// result function invoked
+			c.result();	
+			
+			// add function to calculate sum
+			cout << "Result: " <<
+					c.add() << endl;
+			break;
+		case 2:
+			
+			// sub function to calculate
+			// difference
+			c.result();
+			cout << "Result: " <<
+					c.sub() << endl;
+			break;
+		case 3:
+			c.result();
+			
+			// mul function to calculate product
+			cout << "Result: " <<
+					c.mul() << endl;
+			break;
+		case 4:
+			c.result();
+			
+			// div function to calculate division
+			cout << "Result: " <<
+					c.div() << endl;
+			break;
+		}
+		
+	} while (ch >= 1 && ch <= 4);
+	
+	return 0;
+}
+
