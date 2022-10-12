c69a6251f4f5435ab9b01815285d49429549139c
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-12 19:04:45 +0300

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,89 @@
+# include <iostream> 
+  
+  
+  using  namespace std;
+ 
+  int main () {
+ 
+   int sayi1 =10;
+   int sayi2=4;
+   
+   cout << " Sayi1+Sayi2:"<< sayi1+sayi2<<endl;
+   cout << " Sayi1/Sayi2:"<< sayi1/float(sayi2)<<endl;
+   cout << " Sayi1-Sayi2:"<< sayi1-sayi2<<endl;
+   cout << " Sayı1*Sayı2:"<< sayi1*sayi2<<endl;
+   
+   return 0 ;
+ 
+ 
+ 
+ 
+ }  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
