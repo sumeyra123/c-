1a275c66b54628bb87e824f341b915e8dddebf69
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-12 18:08:35 +0300

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std ;
+
+int main (){
+	// deðiþken tipleri ve deðiþkenler
+	/* int
+	float
+	double
+	char
+	bool
+	*/
+	/*int a =32;
+	float b=3.52;
+	double c=4.546;
+	char d= 'A';
+	bool e =false // false 0 döndürür true 1 döndürür
+	
+	
+	cout << "a deðiþkenin deðeri:"<< a << endl;
+	cout << " b deðiþkenin deðeri:"<< b << endl;
+	cout << "c deðiþkenin deðeri :"<< c << endl;
+	cout << " d deðiþkenin deðeri :"<< d << endl;
+	cout << " e deðiþekenin deðeri :"<< e << endl;*/
+	
+	 int a,b,c,toplam=0;
+	 
+	 
+	 a=4;
+	 b=5;
+	 c=9;
+	 toplam =a+b+c;
+	 
+	 cout << "Toplam :" << toplam << endl ;
+	  
+	return 0;
+}
