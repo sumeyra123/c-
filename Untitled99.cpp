b31cf8e3f7ffaa153cfc13acd534491013651dcd
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-14 21:50:14 +0300

    Add files via upload

---
+++ Untitled99.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	
+	
+	int a,b,c;
+	
+	
+	/*cout << "Birinci sayı giriniz:";
+	cin >> a;
+	
+	
+	cout <<"İkinci sayı giriniz:;";
+	cin >> b;
+	
+	
+	cout << "Üçüncü sayı giriniz:";
+	cin >> c;
+	
+	cout << "Toplamları:" << a+b+c << endl;*/
+	
+	cout << " Sayıları giriniz:";
+	
+	cin >> a >> b >> c; // böyle yaparsan yan yana toplar.
+		
+		
+	cout << "Toplamları:" << a+b+c << endl;
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
