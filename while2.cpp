976b95586e9058f45edf0e4b8a46963afd561a1b
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-14 21:47:21 +0300

    Add files via upload

---
+++ while2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	
+	
+	
+	int i =0;
+	  while (i<10){
+	  	if (i==3 | | i == 5){
+		  
+	  	i++;
+	  	continue;
+	  }
+	  cout << "i:" << i <<endl;
+	  i++;
+    
+}
+
+
