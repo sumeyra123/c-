4c1a2e6459b2be465386c5e79203a308ce8e4cd1
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-14 21:49:57 +0300

    Add files via upload

---
+++ Untitled100.cpp
@@ -0,0 +1,22 @@
+// STRÝNGLER
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	
+	string str1 = "Bu bir stringtir:";
+	string str2 = " Bu da bir stringtir:";
+	
+	
+	string str3 = str1 +str2;
+	
+	cout << "Str1:" << str1 << endl;
+	cout << "Str2:" << str2 << endl;
+	
+	cout << "Str3:" << str3 << endl;
+	
+	return 0;
+}
