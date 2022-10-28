f845edc851fcaf9d215e79ab9e2ada7b97493ed3
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-28 13:50:28 +0300

    Add files via upload

---
+++ array.cpp1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	int matris[3][3]={{1,2,3},{4,5,6},{7,8,9}}; // boyutu belli eder matris. yatay dikey seklinde düşün
+	     cout<<matris[0][1]<<endl;
+	     cout<<matris[2][1]<<endl;
+		 cout<<matris[1][1]<<endl;
+		 
+	return 0;	 
+		 
+}
