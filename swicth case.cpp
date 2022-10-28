93a01b77bb461e31c492751b358756b401a7ae39
sumeyra_nihal GELMEZ <93098831+sumeyra123@users.noreply.github.com>
2022-10-28 14:30:16 +0300

    switch case

---
+++ swicth case.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int islem;
+	
+	cout <<"Ýslem giriniz:";
+	
+	cin >> islem;
+	
+	switch(islem){
+		case 1:
+			cout <<"1.iþlemi giriniz:"<<endl;
+			break;
+		case 2 :
+				cout <<"2.iþlemi giriniz:"<<endl;
+		    break;
+		case 3:
+				cout <<"3.iþlemi giriniz:"<<endl;
+		    break;
+		case 4 :
+				cout <<"4.iþlemi giriniz:"<<endl;
+		    break; 
+		default:
+			cout <<"Gecersiz iþlem girdiniz:"<<endl;
+		   break;	 	    
+		  
+		return 0;	
+	}
+	
+	
+	
+	
+	
+	
+}
